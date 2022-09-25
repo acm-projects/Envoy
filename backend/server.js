f0eafb3418a16cab647b0438ca1f5bdb74133465
iamudyavar <aditya.udyavar@gmail.com>
2022-09-25 13:40:46 -0500

    Added authentication

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log('Hello world')
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const port = process.env.PORT || 5000
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
