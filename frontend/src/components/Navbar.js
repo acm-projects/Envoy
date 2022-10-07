ea2fedfff13d4b0875f5a7738dfffe412fe5575a
Harper Wood <harperwood145@gmail.com>
2022-10-07 18:00:06 -0500

    Minor changes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,27 +4,25 @@ import './styles/Navbar.css'
 
 export default function Navbar() {
     return <nav className="nav">
-            <div className="logo">
-                Envoy
-            </div>
-
-                <ul className='ul'>
-                    <li>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link to='/upload'>Upload</Link>
-                    </li>
-                    <li>
-                        <Link to='/account'>Account</Link>
-                    </li>
-                    <li>
-                        <Link to='/login'>Log in</Link>
-                    </li>
-                    <li>
-                        <Link to='/signup'>Sign up</Link>
-                    </li>
-                </ul>
-
+        <div className="logo">
+            Envoy
+        </div>
+        <ul className='ul'>
+            <li>
+                <Link to='/'>Home</Link>
+            </li>
+            <li>
+                <Link to='/upload'>Upload</Link>
+            </li>
+            <li>
+                <Link to='/account'>Account</Link>
+            </li>
+            <li>
+                <Link to='/login'>Log in</Link>
+            </li>
+            <li>
+                <Link to='/signup'>Sign up</Link>
+            </li>
+        </ul>
     </nav>
 }
