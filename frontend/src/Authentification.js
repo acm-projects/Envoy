8d8bafa22001af25b0fe8a59291c74ac0203639c
muaazabed <muaaz.abed@gmail.com>
2022-10-12 21:24:33 -0500

    goodbye, dear code

--- frontend/src/Authentification.js
+++
@@ -1,9 +0,0 @@
-const Authentification = () => {    
-    return(
-        <div className = 'authentification'>
-            <h1>You are successfully logged in!</h1>
-        </div>
-    );
-}
-
-export default Authentification;
